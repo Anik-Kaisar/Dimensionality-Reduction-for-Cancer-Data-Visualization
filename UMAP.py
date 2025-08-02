e2261b6c851255673ec14d01d25347e71f571015
Anik Kaisar <107605809+Anik-Kaisar@users.noreply.github.com>
2025-08-02 19:35:11 +0600

    Add files via upload

---
+++ UMAP.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+#import umap # pip install umap-learn
+import umap
+import pandas as pd
+import Utils
+def main():
+    print("ss")
+    X, y = Utils.read_data() # cancer data
+    # randomly select 800 samples from dataset
+    print("ss")
+    np.random.seed(100)
+    subsample_idc = np.random.choice(X.shape[0], 800, replace=False)
+    X = X[subsample_idc,:]
+    y = y[subsample_idc]
+    y = np.array([int(lbl) for lbl in y])
+    n_components = 2
+    ump = umap.UMAP(
+        n_neighbors=30,
+        min_dist=0.1,
+        n_components=n_components,
+        metric='euclidean'
+    )
+    print("entered")
+    umap_result = ump.fit_transform(X)
+    print(umap_result.shape) # 2d for visualization
+    plt.title('UMAP')
+    plt.scatter(umap_result[:,0], umap_result[:,1], s= 5, c=y, cmap='Spectral')
+    plt.colorbar(boundaries=np.arange(11) - 0.5).set_ticks(np.arange(len(np.unique(y))))
+    plt.show()
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
